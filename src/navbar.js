7c73e43205511f49b02dfed339b4a36ba18721b7
Mario Campopiano <mcampopiano92@gmail.com>
2021-04-12 18:50:57 -0500

    created a nav bar

---
+++ src/navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+export const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Home</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Clients</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Contractors</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Log out</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+// export default NavBar;
