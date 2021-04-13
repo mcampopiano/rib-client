661b9ffa42e68b5fffb7f8b802b49bb8ee0f26e9
Mario Campopiano <mcampopiano92@gmail.com>
2021-04-12 20:16:23 -0500

    Included logic for redirecting to login or register as appropriate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import "./App.css";
-import {NavBar} from "./navbar";
+import { Login } from "./components/auth/Login";
+import { Register } from "./components/auth/Register";
+import { Redirect, Route } from "react-router";
+import { NavBar } from "./navbar";
 
 function App() {
   return (
     <>
-      <NavBar/>
+      <Route
+        render={() => {
+          if (localStorage.getItem("ribUserId")) {
+            return (
+              <>
+                <NavBar />
+              </>
+            );
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+
+      <Route
+        path="/login"
+        render={() => {
+          if (localStorage.getItem("ribUserId")) {
+            return <Redirect to="/" />;
+          } else {
+            return <Login />;
+          }
+        }}
+      />
+      <Route
+        path="/register"
+        render={() => {
+          if (localStorage.getItem("ribUserId")) {
+            return <Redirect to="/" />;
+          } else {
+            return <Register />;
+          }
+        }}
+      />
     </>
   );
 }
