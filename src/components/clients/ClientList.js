83b381370d28babd72c9f05580f40e19e0cbc15d
Mario Campopiano <mcampopiano92@gmail.com>
2021-04-15 14:44:46 -0500

    created a client list component

---
+++ src/components/clients/ClientList.js
@@ -0,0 +1,20 @@
+import React, { useContext, useEffect } from "react"
+import { ClientContext } from "./ClientProvider"
+
+export const ClientList = () => {
+    const {clients, getClients} = useContext(ClientContext)
+
+    useEffect(() => {
+        getClients()
+    }, [])
+
+    return (
+        <ul>
+            {
+                clients.map(client => {
+                    <li>{client.name}</li>
+                })
+            }
+        </ul>
+    )
+}
