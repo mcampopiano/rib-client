188c0c6150b9769ed0520380f56e2852dbf909fe
Mario Campopiano <mcampopiano92@gmail.com>
2021-04-18 13:05:42 -0500

    added a button to the client list page that routes to the add client form

--- src/components/clients/ClientList.js
+++ src/components/clients/ClientList.js
@@ -1,20 +1,26 @@
-import React, { useContext, useEffect } from "react"
-import { ClientContext } from "./ClientProvider"
+import { Button } from "reactstrap";
+import React, { useContext, useEffect } from "react";
+import { ClientContext } from "./ClientProvider";
+import { useHistory } from "react-router";
 
 export const ClientList = () => {
-    const {clients, getClients} = useContext(ClientContext)
+  const { clients, getClients } = useContext(ClientContext);
+  const history = useHistory();
 
-    useEffect(() => {
-        getClients()
-    }, [])
+  useEffect(() => {
+    getClients();
+  }, []);
 
-    return (
-        <ul>
-            {
-                clients.map(client => (
-                    <li key={client.id}>{client.name}</li>
-                ))
-            }
-        </ul>
-    )
-}
+  return (
+    <>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>{client.name}</li>
+        ))}
+      </ul>
+      <Button onClick={() => history.push("/clients/form")}>
+        Add new client
+      </Button>
+    </>
+  );
+};
