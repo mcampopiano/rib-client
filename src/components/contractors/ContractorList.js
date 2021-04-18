830345093e966983200acfe30d35611cea3298b4
Mario Campopiano <mcampopiano92@gmail.com>
2021-04-18 13:33:07 -0500

    added a button to the contractor list which routes to the Contractor Form

--- src/components/contractors/ContractorList.js
+++ src/components/contractors/ContractorList.js
@@ -1,18 +1,26 @@
+import { Button } from "reactstrap";
 import React, { useContext, useEffect } from "react";
 import { ContractorContext } from "./ContractorProvider";
+import { useHistory } from "react-router";
 
 export const ContractorList = () => {
   const { contractors, getContractors } = useContext(ContractorContext);
+  const history = useHistory();
 
   useEffect(() => {
     getContractors();
   }, []);
 
   return (
-    <ul>
-      {contractors.map((contractor) => (
-        <li key={contractor.id}>{contractor.name}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {contractors.map((contractor) => (
+          <li key={contractor.id}>{contractor.name}</li>
+        ))}
+      </ul>
+      <Button onClick={() => history.push("/contractors/form")}>
+        Add new contractor
+      </Button>
+    </>
   );
 };
